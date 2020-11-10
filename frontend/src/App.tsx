71def13ffb9d5d56db00531c70164427dab921e7
shohei-12 <shoheimaeda10@gmail.com>
2020-11-10 22:56:43 +0900

    Create drawer menu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import { DrawerMenu } from "./components/Drawer";
 import { SignUp } from "./templates";
 import "./assets/style.css";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    main: {
+      padding: "80px 10px 0",
+      [theme.breakpoints.up("md")]: {
+        padding: "80px 10px 0 250px",
+      },
+    },
+  })
+);
+
 const App: React.FC = () => {
+  const classes = useStyles();
+
   return (
     <Router>
-      <main>
+      <DrawerMenu />
+      <main className={classes.main}>
         <Switch>
           <Route exact path="/signup" component={SignUp} />
         </Switch>
---
+++ frontend/src/components/Drawer/DrawerMenu.tsx
@@ -0,0 +1,142 @@
+import React, { useCallback } from "react";
+import { Link } from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import PersonAddIcon from "@material-ui/icons/PersonAdd";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import MenuIcon from "@material-ui/icons/Menu";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    drawer: {
+      [theme.breakpoints.up("md")]: {
+        width: drawerWidth,
+        flexShrink: 0,
+      },
+    },
+    appBar: {
+      [theme.breakpoints.up("md")]: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+      },
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up("md")]: {
+        display: "none",
+      },
+    },
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      width: drawerWidth,
+    },
+  })
+);
+
+const DrawerMenu = () => {
+  const classes = useStyles();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = useCallback(() => {
+    setMobileOpen(!mobileOpen);
+  }, [setMobileOpen, mobileOpen]);
+
+  const closeLessThanMd = useCallback(() => {
+    if (window.innerWidth < 960) {
+      handleDrawerToggle();
+    }
+  }, [handleDrawerToggle]);
+
+  const notSignInList = [
+    {
+      text: "ユーザーの登録",
+      icon: <PersonAddIcon />,
+      path: "/signup",
+    },
+  ];
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <Divider />
+      <List>
+        {notSignInList.map((item, index) => (
+          <Link key={index} to={item.path}>
+            <ListItem button onClick={closeLessThanMd}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Link to="/">
+            <Typography variant="h6">Sparkle</Typography>
+          </Link>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="navigation">
+        <Hidden mdUp>
+          <Drawer
+            variant="temporary"
+            anchor="left"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true,
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden smDown>
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+    </div>
+  );
+};
+
+export default DrawerMenu;
