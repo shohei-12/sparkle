f87edb41047d448aadfc01bc777525ed28557063
shohei-12 <shoheimaeda10@gmail.com>
2020-11-11 16:13:41 +0900

    Add password field to the signup form

--- frontend/src/templates/SignUp.tsx
+++ frontend/src/templates/SignUp.tsx
@@ -6,6 +6,8 @@ import { SecondaryButton, TextInput } from "../components/UIkit";
 type Inputs = {
   name: string;
   email: string;
+  password: string;
+  confirmPassword: string;
 };
 
 const SignUp: React.FC = () => {
@@ -13,11 +15,15 @@ const SignUp: React.FC = () => {
     defaultValues: {
       name: "",
       email: "",
+      password: "",
+      confirmPassword: "",
     },
   });
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const inputName = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -31,6 +37,20 @@ const SignUp: React.FC = () => {
       setEmail(event.target.value);
     },
     [setEmail]
+  );
+
+  const inputPassword = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setPassword(event.target.value);
+    },
+    [setPassword]
+  );
+
+  const inputConfirmPassword = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setConfirmPassword(event.target.value);
+    },
+    [setConfirmPassword]
   );
 
   const signUp = () => {
@@ -50,7 +70,7 @@ const SignUp: React.FC = () => {
       <h2>新規ユーザー登録</h2>
       <TextInput
         fullWidth={true}
-        label="ユーザー名"
+        label="ユーザー名（10文字以内）"
         multiline={false}
         required={true}
         rows="1"
@@ -86,10 +106,59 @@ const SignUp: React.FC = () => {
         helperText={errors.email && errors.email.message}
         onChange={inputEmail}
       />
+      <TextInput
+        fullWidth={true}
+        label="パスワード"
+        multiline={false}
+        required={true}
+        rows="1"
+        type="password"
+        name="password"
+        inputRef={register({
+          required: "入力必須です。",
+          minLength: {
+            value: 6,
+            message: "6文字以上で入力してください。",
+          },
+          pattern: {
+            value: /^[a-zA-Z0-9_-]+$/,
+            message:
+              "半角英数字、ハイフン(-)、アンダーバー(_)のみ利用可能です。",
+          },
+        })}
+        error={Boolean(errors.password)}
+        helperText={
+          errors.password
+            ? errors.password.message
+            : "6文字以上で入力してください。半角英数字、ハイフン(-)、アンダーバー(_)のみ利用可能です。"
+        }
+        onChange={inputPassword}
+      />
+      <TextInput
+        fullWidth={true}
+        label="パスワード（確認）"
+        multiline={false}
+        required={true}
+        rows="1"
+        type="password"
+        name="confirmPassword"
+        inputRef={register({
+          required: "入力必須です。",
+          minLength: {
+            value: 6,
+            message: "6文字以上で入力してください。",
+          },
+          validate: (value) =>
+            value === password || "パスワードが一致しません。",
+        })}
+        error={Boolean(errors.confirmPassword)}
+        helperText={errors.confirmPassword && errors.confirmPassword.message}
+        onChange={inputConfirmPassword}
+      />
       <div className="space-m"></div>
       <SecondaryButton
         text="登録する"
-        disabled={name && email ? false : true}
+        disabled={name && email && password && confirmPassword ? false : true}
         onClick={handleSubmit(() => signUp())}
       />
     </div>
