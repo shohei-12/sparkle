42a44338f2cfc7fcb89f509a236a8d20bb879c50
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2020-11-15 19:35:52 +0900

    Add signin page (#36)

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import { SignUp } from "./templates";
+import { SignIn, SignUp } from "./templates";
 
 const Router: React.FC = () => {
   return (
     <Switch>
+      <Route exact path="/signin" component={SignIn} />
       <Route exact path="/signup" component={SignUp} />
     </Switch>
   );
