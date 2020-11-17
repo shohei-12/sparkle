022079405f322c537a6620feb30d277e3f35f874
shohei-12 <shoheimaeda10@gmail.com>
2020-11-17 22:51:01 +0900

    Keep logged in

--- frontend/src/re-ducks/users/operations.ts
+++ frontend/src/re-ducks/users/operations.ts
@@ -14,6 +14,11 @@ export const signIn = (email: string, password: string) => {
     })
       .then((response) => {
         const responseData = response.data.data;
+        const responseHeaders = response.headers;
+        console.log(responseHeaders);
+        localStorage.setItem("uid", responseData.email);
+        localStorage.setItem("client", responseHeaders.client);
+        localStorage.setItem("access_token", responseHeaders.access_token);
         dispatch(
           signInAction({
             isSignedIn: true,
@@ -26,6 +31,34 @@ export const signIn = (email: string, password: string) => {
       })
       .catch(() => {
         alert("入力されたメールアドレスまたはパスワードに誤りがあります。");
+      });
+  };
+};
+
+export const listenAuthState = () => {
+  return async (dispatch: any) => {
+    axios({
+      method: "GET",
+      url: "http://localhost:80/api/v1/auth/validate_token",
+      params: {
+        uid: localStorage.getItem("uid"),
+        client: localStorage.getItem("client"),
+        access_token: localStorage.getItem("access_token"),
+      },
+    })
+      .then((response) => {
+        const responseData = response.data.data;
+        dispatch(
+          signInAction({
+            isSignedIn: true,
+            id: String(responseData.id),
+            name: responseData.name,
+            email: responseData.email,
+          })
+        );
+      })
+      .catch(() => {
+        dispatch(push("/signin"));
       });
   };
 };
