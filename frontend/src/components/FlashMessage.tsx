c988e6b0207edd5675de35cda90242809e641c6e
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2020-11-20 18:47:28 +0900

    Erase flash message over time (#68)

--- frontend/src/components/FlashMessage.tsx
+++ frontend/src/components/FlashMessage.tsx
@@ -1,16 +1,21 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Alert from "@material-ui/lab/Alert";
 import { Store } from "../re-ducks/store/types";
 import {
   getFlashMessage,
   getFlashMessageType,
 } from "../re-ducks/flash/selectors";
+import { flashAction } from "../re-ducks/flash/actions";
 
 const FlashMessage: React.FC = () => {
+  const dispatch = useDispatch();
   const selector = useSelector((state: Store) => state);
   const msg = getFlashMessage(selector);
   const type = getFlashMessageType(selector);
+
+  type &&
+    window.setTimeout(() => dispatch(flashAction({ type: "", msg: "" })), 5000);
 
   return (
     <>
