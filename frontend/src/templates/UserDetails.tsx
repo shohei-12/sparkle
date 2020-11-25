fe9d083faa3e3857c8711888265e8b3df96186a9
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2020-11-25 13:56:35 +0900

    Add calendar (#86)

--- frontend/src/templates/UserDetails.tsx
+++ frontend/src/templates/UserDetails.tsx
@@ -1,10 +1,12 @@
 import React, { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { push } from "connected-react-router";
+import Calendar from "react-calendar";
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import { getUserName, getUserProfile } from "../re-ducks/users/selectors";
 import { Store } from "../re-ducks/store/types";
 import { SecondaryButton } from "../components/UIkit";
+import "react-calendar/dist/Calendar.css";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -27,6 +29,14 @@ const UserDetails = () => {
     dispatch(push("/user/edit"));
   }, [dispatch]);
 
+  const goDailyRecordPage = useCallback(
+    (value: any) => {
+      console.log(value);
+      dispatch(push("/daily-record"));
+    },
+    [dispatch]
+  );
+
   return (
     <div className="wrap">
       <h2>My Page</h2>
@@ -39,6 +49,11 @@ const UserDetails = () => {
       />
       <span>{name}</span>
       <SecondaryButton text="ユーザー情報を編集する" onClick={goUserEditPage} />
+      <Calendar
+        calendarType="US"
+        value={new Date()}
+        onClickDay={goDailyRecordPage}
+      />
     </div>
   );
 };
