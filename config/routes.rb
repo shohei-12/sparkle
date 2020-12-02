7c9f9d46739d42ea17d7e82620f434f9a6c5c1ca
shohei-12 <shoheimaeda10@gmail.com>
2020-12-02 14:02:07 +0900

    Create detailed page of day record

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for 'User', at: 'auth'
       put '/toggle-theme', to: 'users#toggle_theme'
       post '/records', to: 'records#create'
+      get '/record', to: 'records#show'
       post '/appearances', to: 'appearances#create'
       post '/meals', to: 'meals#create'
       resources :users
