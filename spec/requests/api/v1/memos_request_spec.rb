e0d20b3bcdb7a4790116381f1427aded13e9befe
shohei-12 <shoheimaeda10@gmail.com>
2020-12-09 14:59:03 +0900

    Write request test

--- spec/requests/api/v1/memos_request_spec.rb
+++ spec/requests/api/v1/memos_request_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::Memos', type: :request do
   let(:record) { create(:record) }
+  let(:memo) { create(:memo) }
 
   describe 'POST /api/v1/memos' do
     context 'when data is valid' do
@@ -42,8 +43,6 @@ RSpec.describe 'Api::V1::Memos', type: :request do
   end
 
   describe 'GET /api/v1/memos/:id' do
-    let(:memo) { create(:memo) }
-
     context 'when record exists' do
       context 'when there is corresponding memo in record' do
         it 'get memo' do
@@ -65,6 +64,121 @@ RSpec.describe 'Api::V1::Memos', type: :request do
     context 'when record does not exist' do
       it 'raise ActiveRecord::RecordNotFound' do
         expect { get_memo(record.id + 1) }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+
+  describe 'PUT /api/v1/memos/:id/appearance' do
+    let(:data) do
+      {
+        appearance: 'memo update'
+      }
+    end
+
+    context 'when memo exists' do
+      it 'update appearance memo' do
+        update_appearance(memo.id, data)
+        expect(response.status).to eq 204
+        memo.reload
+        expect(memo.appearance).to eq 'memo update'
+      end
+    end
+
+    context 'when memo does not exist' do
+      it 'raise ActiveRecord::RecordNotFound' do
+        expect { update_appearance(memo.id + 1, data) }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+
+  describe 'PUT /api/v1/memos/:id/breakfast' do
+    let(:data) do
+      {
+        breakfast: 'memo update'
+      }
+    end
+
+    context 'when memo exists' do
+      it 'update breakfast memo' do
+        update_breakfast(memo.id, data)
+        expect(response.status).to eq 204
+        memo.reload
+        expect(memo.breakfast).to eq 'memo update'
+      end
+    end
+
+    context 'when memo does not exist' do
+      it 'raise ActiveRecord::RecordNotFound' do
+        expect { update_breakfast(memo.id + 1, data) }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+
+  describe 'PUT /api/v1/memos/:id/lunch' do
+    let(:data) do
+      {
+        lunch: 'memo update'
+      }
+    end
+
+    context 'when memo exists' do
+      it 'update lunch memo' do
+        update_lunch(memo.id, data)
+        expect(response.status).to eq 204
+        memo.reload
+        expect(memo.lunch).to eq 'memo update'
+      end
+    end
+
+    context 'when memo does not exist' do
+      it 'raise ActiveRecord::RecordNotFound' do
+        expect { update_lunch(memo.id + 1, data) }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+
+  describe 'PUT /api/v1/memos/:id/dinner' do
+    let(:data) do
+      {
+        dinner: 'memo update'
+      }
+    end
+
+    context 'when memo exists' do
+      it 'update dinner memo' do
+        update_dinner(memo.id, data)
+        expect(response.status).to eq 204
+        memo.reload
+        expect(memo.dinner).to eq 'memo update'
+      end
+    end
+
+    context 'when memo does not exist' do
+      it 'raise ActiveRecord::RecordNotFound' do
+        expect { update_dinner(memo.id + 1, data) }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+
+  describe 'PUT /api/v1/memos/:id/snack' do
+    let(:data) do
+      {
+        snack: 'memo update'
+      }
+    end
+
+    context 'when memo exists' do
+      it 'update snack memo' do
+        update_snack(memo.id, data)
+        expect(response.status).to eq 204
+        memo.reload
+        expect(memo.snack).to eq 'memo update'
+      end
+    end
+
+    context 'when memo does not exist' do
+      it 'raise ActiveRecord::RecordNotFound' do
+        expect { update_snack(memo.id + 1, data) }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
--- spec/support/memo_helper.rb
+++ spec/support/memo_helper.rb
@@ -6,4 +6,24 @@ module MemoHelpers
   def get_memo(data)
     get "/api/v1/memos/#{data}"
   end
+
+  def update_appearance(memo_id, data)
+    put "/api/v1/memos/#{memo_id}/appearance", params: data
+  end
+
+  def update_breakfast(memo_id, data)
+    put "/api/v1/memos/#{memo_id}/breakfast", params: data
+  end
+
+  def update_lunch(memo_id, data)
+    put "/api/v1/memos/#{memo_id}/lunch", params: data
+  end
+
+  def update_dinner(memo_id, data)
+    put "/api/v1/memos/#{memo_id}/dinner", params: data
+  end
+
+  def update_snack(memo_id, data)
+    put "/api/v1/memos/#{memo_id}/snack", params: data
+  end
 end
