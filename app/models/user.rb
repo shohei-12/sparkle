5463150699cad2be111a38b0ea638bdb98933dd6
shohei-12 <shoheimaeda10@gmail.com>
2020-12-10 22:05:18 +0900

    Create relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
   mount_uploader :profile, ImageUploader
 
   has_many :records, dependent: :destroy
+  has_many :relationships, dependent: :destroy
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :user
 
   validates :name, presence: true, length: { maximum: 20 }
   validates :email,
