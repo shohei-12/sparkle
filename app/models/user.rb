2bd7a965586d8cd38e9d03ab9b252bfe4ef7a03a
shohei-12 <shoheimaeda10@gmail.com>
2020-12-11 19:44:19 +0900

    Fix rubocop warning

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ApplicationRecord
 
   def unfollow(other_user)
     relationship = relationships.find_by(follow_id: other_user.id)
-    relationship.destroy if relationship
+    relationship&.destroy
   end
 
   def following?(other_user)
