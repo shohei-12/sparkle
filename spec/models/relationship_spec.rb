a2da3f2104a04008da527dc9341067ab1a132929
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2020-12-12 21:52:58 +0900

    Write model test (#175)
    * Write model test
    
    * Fix rubocop warning
    
    * modify test

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:relationship) { build(:relationship) }
+
+  it 'return true' do
+    expect(relationship.valid?).to eq true
+  end
+
+  describe 'user_id' do
+    context 'when empty' do
+      before { relationship.user_id = '' }
+
+      it 'return false' do
+        expect(relationship.valid?).to eq false
+      end
+    end
+
+    context 'when user does not exist' do
+      before { relationship.user_id += 2 }
+
+      it 'return false' do
+        expect(relationship.valid?).to eq false
+      end
+    end
+
+    context 'when duplicate combination of user_id and follow_id' do
+      before do
+        @relationship = create(:relationship)
+        relationship.user_id = @relationship.user_id
+        relationship.follow_id = @relationship.follow_id
+      end
+
+      it 'return false' do
+        expect(relationship.valid?).to eq false
+      end
+    end
+  end
+
+  describe 'follow_id' do
+    context 'when empty' do
+      before { relationship.follow_id = '' }
+
+      it 'return false' do
+        expect(relationship.valid?).to eq false
+      end
+    end
+
+    context 'when user does not exist' do
+      before { relationship.follow_id += 1 }
+
+      it 'return false' do
+        expect(relationship.valid?).to eq false
+      end
+    end
+  end
 end
