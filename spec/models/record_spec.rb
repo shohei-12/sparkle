ddffb5fdc73c7952657b741747c06f5154b4688f
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2020-12-21 15:50:57 +0900

    Write model test (#203)

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -1,48 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe Record, type: :model do
-  let(:record) { build(:record) }
+  let(:record1) { build(:record) }
+  let(:record2) { create(:record) }
+  let(:user) { create(:user) }
 
   it 'return true' do
-    expect(record.valid?).to eq true
+    expect(record1.valid?).to eq true
   end
 
   describe 'date' do
     context 'when empty' do
-      before { record.date = '' }
+      before { record1.date = '' }
 
       it 'return false' do
-        expect(record.valid?).to eq false
+        expect(record1.valid?).to eq false
       end
     end
   end
 
   describe 'user_id' do
     context 'when empty' do
-      before { record.user_id = '' }
+      before { record1.user_id = '' }
 
       it 'return false' do
-        expect(record.valid?).to eq false
+        expect(record1.valid?).to eq false
       end
     end
 
     context 'when user does not exist' do
-      before { record.user_id += 1 }
+      before { record1.user_id += 1 }
 
       it 'return false' do
-        expect(record.valid?).to eq false
+        expect(record1.valid?).to eq false
       end
     end
 
     context 'when duplicate combination of user_id and date' do
       before do
-        @record = create(:record)
-        record.user_id = @record.user_id
-        record.date = @record.date
+        record1.user_id = record2.user_id
+        record1.date = record2.date
       end
 
       it 'return false' do
-        expect(record.valid?).to eq false
+        expect(record1.valid?).to eq false
+      end
+    end
+  end
+
+  describe '#liked_by?(current_user)' do
+    context 'when liked' do
+      before { user.like(record2) }
+
+      it 'return true' do
+        expect(record2.liked_by?(user)).to eq true
+      end
+    end
+
+    context 'when not liked' do
+      it 'return false' do
+        expect(record2.liked_by?(user)).to eq false
       end
     end
   end
