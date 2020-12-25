f2dd50b63024be3cdcd3d88199e5edbfb9bbd532
shohei-12 <shoheimaeda10@gmail.com>
2020-12-25 17:28:09 +0900

    Write request test

--- spec/support/users_helper.rb
+++ spec/support/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def toggle_theme(data)
     put '/api/v1/toggle-theme', params: data
   end
+
+  def get_user(id, token)
+    get "/api/v1/users/#{id}", headers: token
+  end
 end
