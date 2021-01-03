19e23c8c03859cc82c60660f69f9f2c2ce063f9b
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2021-01-03 22:42:26 +0900

    Write request test (#236)

--- spec/requests/api/v1/records_request_spec.rb
+++ spec/requests/api/v1/records_request_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 RSpec.describe 'Api::V1::Records', type: :request do
   let(:user) { create(:user) }
   let(:record) { create(:record) }
+  let(:start0) do
+    { start: 0 }
+  end
+  let(:start20) do
+    { start: 20 }
+  end
 
   describe 'POST /api/v1/records' do
     context 'when data is valid' do
@@ -66,41 +72,89 @@ RSpec.describe 'Api::V1::Records', type: :request do
   end
 
   describe 'GET /api/v1/records' do
-    let(:data1) do
-      {
-        start: 0
-      }
-    end
-
-    let(:data2) do
-      {
-        start: 20
-      }
-    end
-
     context 'when there are more than 20 records' do
       before { create_list(:record, 21) }
 
       it 'get 20 records' do
-        get_20records(data1)
+        get_20_records(start0)
         expect(response.status).to eq 200
         expect(JSON.parse(response.body).length).to eq 20
+      end
+
+      it 'get 1 record' do
+        get_20_records(start20)
+        expect(response.status).to eq 200
+        expect(JSON.parse(response.body).length).to eq 1
       end
     end
 
     context 'when there are less than 20 records' do
-      before { create_list(:record, 3) }
+      before { create_list(:record, 2) }
 
-      it 'get 3 records' do
-        get_20records(data1)
+      it 'get 2 records' do
+        get_20_records(start0)
         expect(response.status).to eq 200
-        expect(JSON.parse(response.body).length).to eq 3
+        expect(JSON.parse(response.body).length).to eq 2
       end
 
       it 'not get record' do
-        get_20records(data2)
+        get_20_records(start20)
         expect(response.status).to eq 200
         expect(JSON.parse(response.body).length).to eq 0
+      end
+    end
+  end
+
+  describe 'GET /api/v1/users/:id/like-records' do
+    context 'when user exists' do
+      before { @user = create(:user) }
+
+      context 'when there are more than 20 like records' do
+        before do
+          21.times do
+            record_n = create(:record)
+            @user.like(record_n)
+          end
+        end
+
+        it 'get 20 like records' do
+          get_20_like_records(start0, @user.id)
+          expect(response.status).to eq 200
+          expect(JSON.parse(response.body).length).to eq 20
+        end
+
+        it 'get 1 like record' do
+          get_20_like_records(start20, @user.id)
+          expect(response.status).to eq 200
+          expect(JSON.parse(response.body).length).to eq 1
+        end
+      end
+
+      context 'when there are less than 20 like records' do
+        before do
+          2.times do
+            record_n = create(:record)
+            @user.like(record_n)
+          end
+        end
+
+        it 'get 2 like records' do
+          get_20_like_records(start0, @user.id)
+          expect(response.status).to eq 200
+          expect(JSON.parse(response.body).length).to eq 2
+        end
+
+        it 'not get like records' do
+          get_20_like_records(start20, @user.id)
+          expect(response.status).to eq 200
+          expect(JSON.parse(response.body).length).to eq 0
+        end
+      end
+    end
+
+    context 'when user does not exist' do
+      it 'raise ActiveRecord::RecordNotFound' do
+        expect { get_20_like_records(start0, user.id + 1) }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
