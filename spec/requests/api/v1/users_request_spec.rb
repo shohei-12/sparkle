2473ab20cdd910e74f9d5f14289b87213ade1678
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2021-01-09 23:18:21 +0900

    Write request test (#264)

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -4,6 +4,13 @@ RSpec.describe 'Api::V1::Users', type: :request do
   let(:user1) { create(:user) }
   let(:user2) { create(:user) }
   let(:record) { create(:record) }
+  let(:token) { sign_in({ email: user1.email, password: 'password' }) }
+  let(:start0) do
+    { start: 0 }
+  end
+  let(:start20) do
+    { start: 20 }
+  end
 
   describe 'GET /api/v1/users/:id' do
     context 'when user exists' do
@@ -57,6 +64,166 @@ RSpec.describe 'Api::V1::Users', type: :request do
         expect(response.status).to eq 204
         user1.reload
         expect(user1.theme).to eq 'light'
+      end
+    end
+  end
+
+  describe 'GET /api/v1/users/:id/followings' do
+    context 'when current user exists' do
+      context 'when user exists' do
+        context 'when there are more than 20 followings' do
+          before do
+            21.times do
+              user = create(:user)
+              user1.follow(user)
+            end
+          end
+
+          it 'get 20 followings' do
+            get_20_followings(user1.id, start0, token)
+            expect(response.status).to eq 200
+            expect(JSON.parse(response.body).length).to eq 20
+          end
+
+          it 'get 1 following' do
+            get_20_followings(user1.id, start20, token)
+            expect(response.status).to eq 200
+            expect(JSON.parse(response.body).length).to eq 1
+          end
+        end
+
+        context 'when there are less than 20 followings' do
+          before do
+            2.times do
+              user = create(:user)
+              user1.follow(user)
+            end
+          end
+
+          it 'get 2 followings' do
+            get_20_followings(user1.id, start0, token)
+            expect(response.status).to eq 200
+            expect(JSON.parse(response.body).length).to eq 2
+          end
+
+          it 'not get followings' do
+            get_20_followings(user1.id, start20, token)
+            expect(response.status).to eq 200
+            expect(JSON.parse(response.body).length).to eq 0
+          end
+        end
+
+        context 'when when followings does not exist' do
+          it 'not get followings' do
+            get_20_followings(user1.id, start0, token)
+            expect(response.status).to eq 200
+            expect(JSON.parse(response.body).length).to eq 0
+          end
+        end
+      end
+
+      context 'when user does not exist' do
+        it 'raise ActiveRecord::RecordNotFound' do
+          expect { get_20_followings(user1.id + 1, start0, token) }.to raise_error(ActiveRecord::RecordNotFound)
+        end
+      end
+    end
+
+    context 'when current user does not exist' do
+      context 'when followings exists' do
+        before { user1.follow(user2) }
+
+        it 'raise NoMethodError' do
+          expect { get_20_followings(user1.id, start0, nil) }.to raise_error(NoMethodError)
+        end
+      end
+
+      context 'when followings does not exist' do
+        it 'not get followings' do
+          get_20_followings(user1.id, start0, nil)
+          expect(response.status).to eq 200
+          expect(JSON.parse(response.body).length).to eq 0
+        end
+      end
+    end
+  end
+
+  describe 'GET /api/v1/users/:id/followers' do
+    context 'when current user exists' do
+      context 'when user exists' do
+        context 'when there are more than 20 followers' do
+          before do
+            21.times do
+              user = create(:user)
+              user.follow(user1)
+            end
+          end
+
+          it 'get 20 followers' do
+            get_20_followers(user1.id, start0, token)
+            expect(response.status).to eq 200
+            expect(JSON.parse(response.body).length).to eq 20
+          end
+
+          it 'get 1 follower' do
+            get_20_followers(user1.id, start20, token)
+            expect(response.status).to eq 200
+            expect(JSON.parse(response.body).length).to eq 1
+          end
+        end
+
+        context 'when there are less than 20 followers' do
+          before do
+            2.times do
+              user = create(:user)
+              user.follow(user1)
+            end
+          end
+
+          it 'get 2 followers' do
+            get_20_followers(user1.id, start0, token)
+            expect(response.status).to eq 200
+            expect(JSON.parse(response.body).length).to eq 2
+          end
+
+          it 'not get followers' do
+            get_20_followers(user1.id, start20, token)
+            expect(response.status).to eq 200
+            expect(JSON.parse(response.body).length).to eq 0
+          end
+        end
+
+        context 'when when followers does not exist' do
+          it 'not get followers' do
+            get_20_followers(user1.id, start0, token)
+            expect(response.status).to eq 200
+            expect(JSON.parse(response.body).length).to eq 0
+          end
+        end
+      end
+
+      context 'when user does not exist' do
+        it 'raise ActiveRecord::RecordNotFound' do
+          expect { get_20_followers(user1.id + 1, start0, token) }.to raise_error(ActiveRecord::RecordNotFound)
+        end
+      end
+    end
+
+    context 'when current user does not exist' do
+      context 'when followers exists' do
+        before { user2.follow(user1) }
+
+        it 'raise NoMethodError' do
+          expect { get_20_followers(user1.id, start0, nil) }.to raise_error(NoMethodError)
+        end
+      end
+
+      context 'when followers does not exist' do
+        it 'not get followers' do
+          get_20_followers(user1.id, start0, nil)
+          expect(response.status).to eq 200
+          expect(JSON.parse(response.body).length).to eq 0
+        end
       end
     end
   end
--- spec/support/users_helper.rb
+++ spec/support/users_helper.rb
@@ -6,4 +6,12 @@ module UsersHelper
   def get_user(id)
     get "/api/v1/users/#{id}"
   end
+
+  def get_20_followings(id, data, token)
+    get "/api/v1/users/#{id}/followings", params: data, headers: token
+  end
+
+  def get_20_followers(id, data, token)
+    get "/api/v1/users/#{id}/followers", params: data, headers: token
+  end
 end
