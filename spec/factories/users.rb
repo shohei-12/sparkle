20e109a2770e4695b7efd881721885d07a23f3f7
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2021-01-21 22:08:42 +0900

    Add and update test (#313)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     name { 'test' }
     sequence(:email) { |n| "test#{n}@example.com" }
+    self_introduction { 'This is a test.' }
     password { 'password' }
     password_confirmation { 'password' }
     theme { 'light' }
