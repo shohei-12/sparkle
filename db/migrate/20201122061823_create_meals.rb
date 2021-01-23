0b3171eb266cfead02992bd6ccb7856b356cdca3
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2021-01-23 23:10:15 +0900

    Delete eating times table (#321)
    * Write request test
    
    * Delete eating_times table
    
    * Fix test

--- db/migrate/20201122061823_create_meals.rb
+++ db/migrate/20201122061823_create_meals.rb
@@ -2,9 +2,8 @@ class CreateMeals < ActiveRecord::Migration[6.0]
   def change
     create_table :meals do |t|
       t.string :image
-      t.time :time
+      t.string :meal_type, null: false
       t.references :record, null: false, foreign_key: true
-      t.references :eating_time, null: false, foreign_key: true
 
       t.timestamps
     end
--- spec/factories/meals.rb
+++ spec/factories/meals.rb
@@ -1,8 +1,7 @@
 FactoryBot.define do
   factory :meal do
     image { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/test.jpg'), 'image/jpeg') }
-    time { '2020-11-22 15:18:23' }
+    meal_type { 'breakfast' }
     association :record
-    association :eating_time
   end
 end
