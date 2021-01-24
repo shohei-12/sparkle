caafac1425e6ffc5a4cfd3d6e1f715c5116394b0
shohei-12 <shoheimaeda10@gmail.com>
2021-01-24 20:21:37 +0900

    Write model test

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -107,4 +107,62 @@ RSpec.describe Record, type: :model do
       end
     end
   end
+
+  describe '#self.slice_images(images)' do
+    context 'when images exists' do
+      before { @appearances = create_list(:appearance, 2) }
+
+      it 'slice images' do
+        result = described_class.slice_images(@appearances)
+        expect(result).to eq [
+          [@appearances[0].id, @appearances[0].image.url],
+          [@appearances[1].id, @appearances[1].image.url]
+        ]
+      end
+    end
+
+    context 'when images does not exist' do
+      it 'raise NoMethodError' do
+        expect { described_class.slice_images(nil) }.to raise_error(NoMethodError)
+      end
+    end
+  end
+
+  describe '#self.delete_images(appearances, meals, current_user_id)' do
+    before do
+      record = create(:record, user_id: user.id)
+      @appearance1 = create(:appearance, record_id: record.id)
+      @appearance2 = create(:appearance, record_id: record.id)
+      @meal1 = create(:meal, record_id: record.id)
+      @meal2 = create(:meal, record_id: record.id)
+    end
+
+    context 'when images belongs to me' do
+      it 'delete images' do
+        expect(Appearance.count).to eq 2
+        expect(Meal.count).to eq 2
+        described_class.delete_images(
+          [@appearance1.id, @appearance2.id],
+          [@meal1.id, @meal2.id],
+          user.id
+        )
+        expect(Appearance.count).to eq 0
+        expect(Meal.count).to eq 0
+      end
+    end
+
+    context 'when images belongs to someone else' do
+      it 'not delete images' do
+        expect(Appearance.count).to eq 2
+        expect(Meal.count).to eq 2
+        described_class.delete_images(
+          [@appearance1.id, @appearance2.id],
+          [@meal1.id, @meal2.id],
+          user.id + 1
+        )
+        expect(Appearance.count).to eq 2
+        expect(Meal.count).to eq 2
+      end
+    end
+  end
 end
