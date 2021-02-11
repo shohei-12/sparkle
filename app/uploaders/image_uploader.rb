a791c3d256b2bb72f257fd6b05837933116ad94f
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2021-02-11 22:25:07 +0900

    Do not resize images when uploading (#404)

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
-
-  process resize_to_fit: [350, 350]
+  # include CarrierWave::MiniMagick
 
   if Rails.env.development? || Rails.env.test?
     storage :file
