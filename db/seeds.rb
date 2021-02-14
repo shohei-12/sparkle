f50176d6a2162fd42e46b364497c8eb9e4d149fb
shohei-12 <shoheimaeda10@gmail.com>
2021-02-14 21:20:33 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,30 @@
 # guest user
 User.create!(
-  profile: open("#{Rails.root}/db/fixtures/guest-profile.jpg"),
+  profile: open("#{Rails.root}/db/fixtures/user/guest.jpg"),
   name: 'ゲスト',
   email: 'guest@example.com',
   password: 'password',
   password_confirmation: 'password',
   theme: 'light'
+  self_introduction: 'ゲストユーザーです。'
 )
 
 # sub user
-50.times do |n|
+4.times do |n|
   User.create!(
+    profile: open("#{Rails.root}/db/fixtures/user/sub#{n + 1}.jpg"),
     name: Faker::Name.unique.first_name,
     email: "sub#{n + 1}@example.com",
     password: 'password',
     password_confirmation: 'password',
     theme: 'light'
+    self_introduction: "ユーザー#{n + 1}です。"
   )
 end
 
 # relationships
-51.times do |n|
-  51.times do |i|
+5.times do |n|
+  5.times do |i|
     next unless (n + 1) != (i + 1)
 
     Relationship.create!(
@@ -32,10 +35,10 @@ end
 end
 
 # records
-head = Date.new(2021, 1, 1)
+head = Date.new(2021, 2, 1)
 tail = Date.new(2021, 2, 28)
 (head..tail).each do |d|
-  51.times do |n|
+  5.times do |n|
     Record.create!(
       date: d,
       user_id: n + 1
@@ -44,7 +47,7 @@ tail = Date.new(2021, 2, 28)
 end
 
 # likes
-51.times do |n|
+5.times do |n|
   50.times do |i|
     Like.create!(
       user_id: n + 1,
@@ -54,7 +57,7 @@ end
 end
 
 # memos
-3009.times do |n|
+140.times do |n|
   Memo.create!(
     appearance: '見た目についてメモを残せます。',
     breakfast: '朝食についてメモを残せます。',
@@ -66,20 +69,20 @@ end
 end
 
 # appearances
-3009.times do |n|
+140.times do |n|
   rand(0..5).times do
     Appearance.create!(
-      image: open("#{Rails.root}/db/fixtures/appearances/appearance#{rand(1..5)}.jpg"),
+      image: open("#{Rails.root}/db/fixtures/appearances/appearance#{rand(1..2)}.jpg"),
       record_id: n + 1
     )
   end
 end
 
 # meals
-3009.times do |i|
+140.times do |i|
   rand(0..3).times do |n|
     Meal.create!(
-      image: open("#{Rails.root}/db/fixtures/breakfasts/meal#{n + 1}.jpg"),
+      image: open("#{Rails.root}/db/fixtures/meal/meal.jpg"),
       meal_type: 'breakfast',
       record_id: i + 1
     )
@@ -87,7 +90,7 @@ end
 
   rand(0..3).times do |n|
     Meal.create!(
-      image: open("#{Rails.root}/db/fixtures/lunchs/meal#{n + 1}.jpg"),
+      image: open("#{Rails.root}/db/fixtures/meal/meal.jpg"),
       meal_type: 'lunch',
       record_id: i + 1
     )
@@ -95,7 +98,7 @@ end
 
   rand(0..3).times do |n|
     Meal.create!(
-      image: open("#{Rails.root}/db/fixtures/dinners/meal#{n + 1}.jpg"),
+      image: open("#{Rails.root}/db/fixtures/meal/meal.jpg"),
       meal_type: 'dinner',
       record_id: i + 1
     )
@@ -103,7 +106,7 @@ end
 
   rand(0..3).times do |n|
     Meal.create!(
-      image: open("#{Rails.root}/db/fixtures/snacks/meal#{n + 1}.jpg"),
+      image: open("#{Rails.root}/db/fixtures/meal/meal.jpg"),
       meal_type: 'snack',
       record_id: i + 1
     )
