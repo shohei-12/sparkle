3d409aac1a580c2e62ea8eece937cf56bd8f4a00
shohei-12 <shoheimaeda10@gmail.com>
2021-02-15 12:33:17 +0900

    Change display order of top page to descending order

--- app/controllers/api/v1/records_controller.rb
+++ app/controllers/api/v1/records_controller.rb
@@ -13,13 +13,13 @@ class Api::V1::RecordsController < ApplicationController
 
   def index
     # Get 20 cases of data
-    twenty_records = Record.limit(20).offset(params[:start])
+    twenty_records = Record.limit(20).offset(params[:start]).order(created_at: 'DESC')
     record_infos = Record.get_record_infos(twenty_records, current_api_v1_user)
     render json: record_infos
   end
 
   def twenty_like_records
-    twenty_like_records = @user.like_records.limit(20).offset(params[:start])
+    twenty_like_records = @user.like_records.limit(20).offset(params[:start]).order(created_at: 'DESC')
     record_infos = Record.get_record_infos(twenty_like_records, current_api_v1_user)
     render json: record_infos
   end
